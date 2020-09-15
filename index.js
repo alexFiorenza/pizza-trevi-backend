0342a927790b210aa4c74cd0f44c33b9a3c04e7c
alexFiorenza <alexxdfiorenza@gmail.com>
2020-09-15 19:02:52 -0300

    Deploy heroku

--- index.js
+++ index.js
@@ -13,13 +13,11 @@ const cors = require('cors');
 const socketIo = require('socket.io');
 require('dotenv').config();
 var uri;
-console.log(PORT);
 if (PORT !== 3000) {
   uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@pizza-in-trevi.kqqrm.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
 } else {
   uri = 'mongodb://localhost:27017';
 }
-console.log(process.env.DB_USER);
 /*CORS*/
 app.use(cors());
 /*Middlewares*/
